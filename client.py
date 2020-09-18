6369c07c1a309e216af59c9275d79cceb1c9eee1
Thomas J. Lauer <thomas.lauer@mnsu.edu>
2020-09-18 09:56:23 -0600

    Alpha v0.3.1

--- client.py
+++ client.py
@@ -1,72 +1,125 @@
 import pygame
 from network import Network
 
-
+# Define window width and height (in pixels)
 width = 1280
 height = 720
 
+# Initialize the font functionality of pygame
 pygame.font.init()
+# Create a font for text using Times New Roman size 30
 myfont = pygame.font.SysFont('Times New Roman', 30)
 
+# Create the pygame window with the previously defined width and height
 win = pygame.display.set_mode((width, height))
+# Set the window title to "Client" as a placeholder
 pygame.display.set_caption("Client")
 
 
+# This function receives the array "p2" (containing the character data for all players) send from the server.
+# It loops through all array elements and draws each player character onto the pygame window.
 def redrawWindow(p2):
+    # Set the window background to white
     win.fill((255, 255, 255))
 
+    # Draw the black platform onto the window
     pygame.draw.rect(win, (0, 0, 0), (256, 519, 768, 50))
 
+    # Loop through all elements in the "p2" array as "player"
     for player in p2:
+        # Call the player class function "draw" for the current character
         player.draw(win, myfont)
 
+    # Once everything has been redrawn onto the window, show it to the user
     pygame.display.update()
 
 
+# This is the main client function which runs all client side functions, which is why it is called "main"
 def main():
+    # Initialize the while-loop conditional variable as True
     run = True
+
+    # Get the "Network" class from "network.py" to communicate with the server
     n = Network()
+
+    # Get this specific client's player data, i.e. the data sent to the client when it first connected
     p = n.getP()
+
+    # Set the window title permanently to "Player #" to replace the placeholder, where # is this client's indx number plus 1
     pygame.display.set_caption("Player "+str(p.indx + 1))
+
+    # Initiate the pygame clock, which is used to lock the window frame rate
     clock = pygame.time.Clock()
 
+    # Start the while loop
     while run:
+        # The method "clock.tick()" computes how many milliseconds have passed since the previous call
+        # By calling the method "clock.tick(60)" with the added argument, the method will delay to keep the game running SLOWER than 60 FPS
         clock.tick(60)
 
+        # Send this client's character data to the server. It will reply with an array of data for ALL characters, which we store as "p2"
         p2 = n.send(p)
 
+        # Create a temporary variable to store whether or not this client's character has gotten kicked and in which direction
         playerKicked = 0
 
+        # Loop through all characters in the array "p2" as "player"
         for player in p2:
+            # If the current character has kicked to the LEFT
             if player.kick == 1:
+                # Check to see if this client's character is inside of the kick zone
                 if ((player.x - player.width) <= (p.x + p.width) < player.x) and ((p.y <= player.y + player.height) and (p.y + p.height) >= player.y):
+                    # This client's character is, indeed, inside of the kick zone. Update the temporary variable, which will be used when updating this client's character's position
                     playerKicked = 1
+            # If the current character has kicked UP
             if player.kick == 2:
+                # Check to see if this client's character is inside of the kick zone
                 if ((p.x <= player.x + player.width) and (p.x + p.width) >= player.x) and ((player.y - player.height) <= (p.y + p.height) < player.y):
+                    # This client's character is, indeed, inside of the kick zone. Update the temporary variable, which will be used when updating this client's character's position
                     playerKicked = 2
+            # If the current character has kicked to the RIGHT
             if player.kick == 3:
+                # Check to see if this client's character is inside of the kick zone
                 if ((player.x + player.width) <= p.x < (player.x + (2 * player.width))) and ((p.y <= player.y + player.height) and (p.y + p.height) >= player.y):
+                    # This client's character is, indeed, inside of the kick zone. Update the temporary variable, which will be used when updating this client's character's position
                     playerKicked = 3
 
+        # Move the character by calling the player class function "move" for the current character and passing it the temporary "playerKicked" variable
         p.move(playerKicked)
+
+        # Check whether or not to perform a kick with this client's character by calling the player class function "kickAction"
         p.kickAction()
 
+        # Call the "redrawWindow" function and pass it the information sent by the server, which is stored in the "p2" variable
         redrawWindow(p2)
 
+        # Get a dictionary of all keyboard keys that contains if the specified key is currently pressed (True) or not (False)
         keys = pygame.key.get_pressed()
 
+        # If the keyboard "ESCAPE" key is pressed
         if keys[pygame.K_ESCAPE]:
+            # Set the while-loop conditional variable to False
             run = False
+            # Call the pygame "quit" method to close the window
             pygame.quit()
 
+        # Get all pygame events
         for event in pygame.event.get():
+            # If the "pygame.QUIT" event is found
             if event.type == pygame.QUIT:
+                # Set the while-loop conditional variable to False
                 run = False
+                # Call the pygame "quit" method to close the window
                 pygame.quit()
+                # Break out of the current for-loop
                 break
 
+        # If the current kick value is different from that of the previous frame
         if p.kick != p.kickCheck:
+            # Update kickCheck so that it matches. This check variable is used so that a character kick event is only
+            # registered ONCE when the key is pressed, instead of repeating while the key is help down.
             p.kickCheck = p.kick
 
 
+# Now that all functions have been compiled by the interpreter, call the "main" function to run the game.
 main()
--- player.py
+++ player.py
@@ -2,99 +2,183 @@ import pygame
 import random
 
 
+# Create the "Player" class. This is the code that every character runs.
 class Player:
+    # Initialize the character with data sent by the server when the client first connected
     def __init__(self, indx, x, y, width, height, color, kick, kickCheck):
+        # The player index and the position in the "p2" array of the current character
         self.indx = indx
+        # The X-Axis position of the top-left corner of the character for the initial spawn
         self.x = x
+        # The Y-Axis position of the top-left corner of the character for the initial spawn.
+        # NOTE: The Y-Axis is weird. The top of the window is 0, and increases as you move towards to bottom of the window.
         self.y = y
+        # The width of the character and it's kick zone
         self.width = width
+        # The height of the character and it's kick zone
         self.height = height
+        # The random color assigned to the character
         self.color = color
+        # This is the actual shape of the character itself. This is used when drawing the character onto the window.
         self.rect = (x, y, width, height)
+        # Stores whether or not the character is currently kicking
         self.kick = kick
+        # Stores the value of kick from the previous frame
         self.kickCheck = kickCheck
+        # This is the downward acceleration of the characters
         self.gravity = 0.25
+        # This is the maximum velocity a character can travel along the X-Axis. Possible X-Axis velocities are [-velXMax, velXMax]
         self.velXMax = 12
+        # This is the maximum velocity a character can travel along the Y-Axis. Possible Y-Axis velocities are [-velYMax, velYMax]
         self.velYMax = 10
+        # If the magnitude of the character velocity falls below this threshold, the velocity is set to zero. This prevents drifting.
         self.velThresh = 0.05
+        # This is the value added to / subtracted from the X-Axis Velocity while the left or right keys are being pressed.
         self.acc = 0.5
+        # This is the rate at which the character will slow down along the X-Axis. Think of it as friction from the floor.
         self.accDrag = 0.1
+        # This stores the current Y-Axis velocity of the character.
         self.velY = 0
+        # This stores the current X-Axis velocity of the character.
         self.velX = 0
-        self.velXKick = 0
 
+    # Function used to draw the character itself, as well as a box showing the kick zone if the character is currently kicking
     def draw(self, win, myfont):
+        # Draw the character onto the window "win" with the color "self.color" as specified in "self.rect"
         pygame.draw.rect(win, self.color, self.rect)
+
+        # If the character is currently kicking to the LEFT
         if self.kick == 1:  # print("Kick Left")
+            # Draw the kick zone to the LEFT of the character
             pygame.draw.rect(win, (0, 0, 0), (self.x - self.width, self.y, self.width, self.height))
-        elif self.kick == 2:  # print("Kick Up")
+
+        # If the character is currently kicking UP
+        if self.kick == 2:  # print("Kick Up")
+            # Draw the kick zone ABOVE the character
             pygame.draw.rect(win, (0, 0, 0), (self.x, self.y - self.height, self.width, self.height))
-        elif self.kick == 3:  # print("Kick Right")
+
+        # If the character is currently kicking to the RIGHT
+        if self.kick == 3:  # print("Kick Right")
+            # Draw the kick zone to the RIGHT of the character
             pygame.draw.rect(win, (0, 0, 0), (self.x + self.width, self.y, self.width, self.height))
 
+        # Add black text of the character's (indx+1) using "myfont" on top of the character
         win.blit(myfont.render(str(self.indx + 1), False, (0, 0, 0)), (self.x, self.y))
 
+    # Function does all of the fun math on how the physically move the character, as well as pixel-perfect collision detection for the platform
     def move(self, playerKicked):
 
+        # If the character has been kicked to the LEFT, set it's X-Axis velocity to MAX in the LEFT direction.
         if playerKicked == 1:
             self.velX = -self.velXMax
-        elif playerKicked == 2:
+
+        # If the character has been kicked UP, set it's Y-Axis velocity to MAX in the UPWARDS direction.
+        if playerKicked == 2:
             self.velY = -self.velYMax
-        elif playerKicked == 3:
+
+        # If the character has been kicked to the RIGHT, set it's X-Axis velocity to MAX in the RIGHT direction.
+        if playerKicked == 3:
             self.velX = self.velXMax
 
+        # Get a dictionary of all keyboard keys that contains if the specified key is currently pressed (True) or not (False)
         keys = pygame.key.get_pressed()
 
+        # If the "A" key is being pressed, accelerate to the left by subtracting from the X-Axis velocity
         if keys[pygame.K_a]:  # K_LEFT
             self.velX -= self.acc
+
+        # If the "D" key is being pressed, accelerate to the right by adding to the X-Axis velocity
         if keys[pygame.K_d]:  # K_RIGHT
             self.velX += self.acc
 
+        # NOTE: By having the "A" and "D" keypress checks as separate if statements, they cancel each other out if both are being pressed.
+        #       This results in zero net-change in X-Axis velocity.
+
+        # If both "A" and "D" are being pressed, OR if neither "A" and "D" are being pressed
         if (keys[pygame.K_a] and keys[pygame.K_a]) or (~keys[pygame.K_a] and ~keys[pygame.K_a]):
+            # If the X-Axis velocity is towards the right (positive)
             if self.velX > self.velThresh:
+                # Apply drag to the left (subtract) to slow down the character
                 self.velX -= self.accDrag
+
+            # If the X-Axis velocity is towards the left (negative)
             elif self.velX < -self.velThresh:
+                # Apply drag to the right (add) to slow down the character
                 self.velX += self.accDrag
+
+            # If the magnitude of the X-Axis velocity is below the threshold, set the X-Axis velocity to zero.
             else:
                 self.velX = 0
 
+        # If the Y-Axis velocity is zero (i.e. character is on the platform) and the space bar is pressed
         if keys[pygame.K_SPACE] and self.velY == 0:
+            # Make the character jump by setting the Y-Axis velocity
             self.velY -= (1.25 * self.velYMax)
 
+        # Add gravity to the Y-Axis velocity
         self.velY += self.gravity
 
+        # If the X-Axis velocity was calculated to go above MAX
         if self.velX > self.velXMax:
+            # Set X-Axis velocity to MAX
             self.velX = self.velXMax
+
+        # If the X-Axis velocity was calculated to go below -MAX
         elif self.velX < -self.velXMax:
+            # Set X-Axis velocity to -MAX
             self.velX = -self.velXMax
 
+        # If the character is within the bounds of the platform on the X-Axis
         if 256 <= (self.x + self.width + self.velX) and (self.x + self.velX) <= 1024:
+            # If the character is above, contacting, or will contact the platform on the Y-Axis
             if (self.y + self.height) <= 519 < (self.y + self.height + self.velY):
+                # Set the Y-Axis velocity to zero
                 self.velY = 0
+                # Set the character's Y-Axis position to perfectly stand on top of the platform
                 self.y = 519 - self.height
 
+        # Add the X-Axis velocity to the character's X-Axis position
         self.x += self.velX
+
+        # Add the Y-Axis velocity to the character's Y-Axis position
         self.y += self.velY
 
+        # If the character's Y-Axis position falls below the bounds of the window (i.e. the character falls/is kicked off of the platform)
+        # reset the X-Axis and Y-Axis velocities and respawn the character
         if self.y > 720:
             self.y = -50
             self.x = random.randint(256, 1024)
             self.velX = 0
             self.velY = 0
 
+        # Call the player class function "update" to set the new position of the shape of the character itself
         self.update()
 
+    # Function checks whether or not the character performs a kick
     def kickAction(self):
+        # Get a dictionary of all keyboard keys that contains if the specified key is currently pressed (True) or not (False)
         keys = pygame.key.get_pressed()
 
+        # If the left arrow key is being pressed AND the up arrow key is not being pressed AND the right arrow key is not being pressed
         if keys[pygame.K_LEFT] and not(keys[pygame.K_UP]) and not(keys[pygame.K_RIGHT]):
+            # Update the "kick" variable to signify a kick to the left
             self.kick = 1
+
+        # If the left arrow key is not being pressed AND the up arrow key is not being pressed AND the right arrow key is being pressed
         elif not(keys[pygame.K_LEFT]) and not(keys[pygame.K_UP]) and keys[pygame.K_RIGHT]:
+            # Update the "kick" variable to signify a kick to the right
             self.kick = 3
+
+        # If the left arrow key is not being pressed AND the up arrow key is being pressed AND the right arrow key is not being pressed
         elif not(keys[pygame.K_LEFT]) and keys[pygame.K_UP] and not(keys[pygame.K_RIGHT]):
+            # Update the "kick" variable to signify a kick upwards
             self.kick = 2
+
+        # If the previous three conditional statements to perform a kick all return False
         else:
+            # Update the "kick" variable to signify no kick is being performed
             self.kick = 0
 
+    # Function updates the character shape to the new position of the character
     def update(self):
         self.rect = (self.x, self.y, self.width, self.height)
